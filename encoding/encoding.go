9655508d3dfc6dae7d60f19c8d2ff15d5d3da938
Maksim Pozharskiy <ma_pozharskiy@betboom.org>
2023-11-30 21:18:37 +0300

    add endcoding

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,13 @@
 package encoding
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,16 +31,78 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+	var dockerCompose models.DockerCompose
+
+	jsonFile, err := os.Open(j.FileInput)
+
+	if err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return fmt.Errorf("read file: %w", err)
+	}
+
+	if err = json.Unmarshal(byteValue, &dockerCompose); err != nil {
+		return fmt.Errorf("unmarshal: %w", err)
+	}
+
+	yamlData, err := yaml.Marshal(&dockerCompose)
+	if err != nil {
+		return fmt.Errorf("marshal: %w", err)
+	}
+
+	yamlFile, err := os.Create(j.FileOutput)
+	if err != nil {
+		return fmt.Errorf("create file: %w", err)
+	}
+
+	_, err = yamlFile.Write(yamlData)
+	if err != nil {
+		return fmt.Errorf("write file: %w", err)
+	}
 
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+	var dockerCompose models.DockerCompose
+
+	yamlFile, err := os.Open(y.FileInput)
+
+	if err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+
+	defer yamlFile.Close()
+
+	byteValue, err := io.ReadAll(yamlFile)
+	if err != nil {
+		return fmt.Errorf("read file: %w", err)
+	}
+
+	if err = yaml.Unmarshal(byteValue, &dockerCompose); err != nil {
+		return fmt.Errorf("unmarshal: %w", err)
+	}
+
+	jsonData, err := json.Marshal(&dockerCompose)
+	if err != nil {
+		return fmt.Errorf("marshal: %w", err)
+	}
+
+	jsonFile, err := os.Create(y.FileOutput)
+	if err != nil {
+		return fmt.Errorf("create file: %w", err)
+	}
+
+	_, err = jsonFile.Write(jsonData)
+	if err != nil {
+		return fmt.Errorf("write file: %w", err)
+	}
 
 	return nil
 }
