01d2a9dde25fb871ef4dd9d17218ffd9eb0fb2f9
lightspot <zinnurshamsutdinov@yandex.ru>
2023-12-02 11:26:02 +0300

    Реализовано решение сервис для конвертации данных

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,10 @@
 package encoding
 
 import (
+	"encoding/json"
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
+	"os"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,16 +28,63 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+
+	var dockerCompose models.DockerCompose
+	// Прочитать данные из JSON файла
+	jsonFile, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		return err
+	}
+
+	// Распаковать JSON данные
+	err = json.Unmarshal(jsonFile, &dockerCompose)
+	if err != nil {
+		return err
+	}
+
+	// Преобразовать данные в YAML формат
+	yamlData, err := yaml.Marshal(&dockerCompose)
+	if err != nil {
+		return err
+	}
+
+	// Записать YAML данные в файл
+	err = os.WriteFile(j.FileOutput, yamlData, 0644)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+
+	var dockerCompose models.DockerCompose
+
+	// Открыть YAML файл для чтения
+	yamlFile, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		return err
+	}
+
+	// Распаковать YAML данные
+	err = yaml.Unmarshal(yamlFile, &dockerCompose)
+	if err != nil {
+		return err
+	}
+
+	// Преобразовать данные в JSON формат
+	jsonData, err := json.Marshal(&dockerCompose)
+	if err != nil {
+		return err
+	}
+
+	// Записать JSON данные в файл
+	err = os.WriteFile(y.FileOutput, jsonData, 0644)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
