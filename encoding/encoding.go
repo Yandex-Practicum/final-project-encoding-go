dc02f3f7d1707dbe49401713c2ad44aebb8a826e
RodionIK <82808725+RodionIK@users.noreply.github.com>
2023-12-06 11:41:02 +0500

    Исправление после ревью 2

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -31,64 +31,44 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	//
-	/* Комментарий студента:
-	* Err ладно, она не нужна дальше своего блока
-	* а вот как не выносить Data, если в блоке ReadFile обьявляется
-	* то в блоке Unmarshal попросту не видна
-	* отсюда и начал выносить
-	* Получается надо вносить блок в блок или делать else,
-	* что кажется намного хуже чем выносить переменную
-	if bytesJson, err := os.ReadFile(j.FileInput); err != nil {
+	bytesJson, err := os.ReadFile(j.FileInput)
+	if err != nil {
 		return errors.New(fmt.Sprintf("Не могу открыть файл: %s, %s", j.FileInput, err.Error()))
 	}
 	// десериализуем JSON в DockerCompose
-	if err := json.Unmarshal(bytesJson, j.DockerCompose); err != nil {
-		return errors.New("Ошибка десериализации файла: " + j.FileInput)
-	}
-	*/
-	var data []byte
-	var err error
-	//
-	if data, err = os.ReadFile(j.FileInput); err != nil {
-		return errors.New(fmt.Sprintf("Не могу открыть файл: %s, %s", j.FileInput, err.Error()))
-	}
-	// десериализуем JSON в DockerCompose
-	if err := json.Unmarshal(data, &j.DockerCompose); err != nil {
+	if err = json.Unmarshal(bytesJson, &j.DockerCompose); err != nil {
 		return errors.New("Ошибка десериализации файла: " + j.FileInput)
 	}
 	// сериализация в Yaml
-	if data, err = yaml.Marshal(j.DockerCompose); err != nil {
+	bytesYaml, err := yaml.Marshal(j.DockerCompose)
+	if err != nil {
 		return errors.New(fmt.Sprintf("Ошибка сериализации Yaml: %s", err.Error()))
 	}
 	//
-	if err = os.WriteFile(j.FileOutput, data, 0666); err != nil {
+	if err = os.WriteFile(j.FileOutput, bytesYaml, 0666); err != nil {
 		return errors.New(fmt.Sprintf("yaml file '%s' creation fail: %s", j.FileOutput, err.Error()))
 	}
-
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	var data []byte
-	var err error
-	//
-	if data, err = os.ReadFile(y.FileInput); err != nil {
+	bytesYaml, err := os.ReadFile(y.FileInput)
+	if err != nil {
 		return errors.New(fmt.Sprintf("Не могу открыть файл: %s, %s", y.FileInput, err.Error()))
 	}
 	// десериализуем Yaml в DockerCompose
-	if err = yaml.Unmarshal(data, &y.DockerCompose); err != nil {
+	if err = yaml.Unmarshal(bytesYaml, &y.DockerCompose); err != nil {
 		return errors.New("Ошибка десериализации файла: " + y.FileInput)
 	}
 	// сериализация в Json
-	if data, err = json.Marshal(y.DockerCompose); err != nil {
+	bytesJson, err := json.Marshal(y.DockerCompose)
+	if err != nil {
 		return errors.New(fmt.Sprintf("Ошибка сериализации Yaml: %s", err.Error()))
 	}
 	//
-	if err = os.WriteFile(y.FileOutput, data, 0666); err != nil {
+	if err = os.WriteFile(y.FileOutput, bytesJson, 0666); err != nil {
 		return errors.New(fmt.Sprintf("json file '%s' creation fail: %s", y.FileOutput, err.Error()))
 	}
-
 	return nil
 }
