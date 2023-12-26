8f5a51ce4174ee610011e4f0983ce74106e94eb7
Dranishnikov Nikita <dranishnikov.ns@yandex.ru>
2023-12-26 05:45:26 +0300

    правки после ревью

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -2,6 +2,7 @@ package encoding
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
 	"gopkg.in/yaml.v3"
 	"io"
@@ -29,40 +30,42 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
 	jsonFile, err := os.Open(j.FileInput)
 	if err != nil {
+		fmt.Println("Не удалось открыть файл")
 		return err
 	}
 	defer func(jsonFile *os.File) {
 		err := jsonFile.Close()
 		if err != nil {
-
+			fmt.Println("Не удалось закрыть файл")
 		}
 	}(jsonFile)
 	byteValue, _ := io.ReadAll(jsonFile)
 	err = json.Unmarshal(byteValue, &j.DockerCompose)
 	if err != nil {
+		fmt.Println("Не удалось провести десериализацию")
 		return err
 	}
 	file, err := os.Create(j.FileOutput)
 	if err != nil {
-		os.Exit(1)
+		fmt.Println("Не удалось создать файл")
 		return err
 	}
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
-
+			fmt.Println("Не удалось закрыть файл")
 		}
 	}(file)
 	yamlData, err := yaml.Marshal(j.DockerCompose)
 	if err != nil {
+		fmt.Println("Не удалось провести сериализацию")
 		return err
 	}
 	_, err = file.Write(yamlData)
 	if err != nil {
+		fmt.Println("Не удалось записать данные в файл")
 		return err
 	}
 	return nil
@@ -74,36 +77,40 @@ func (y *YAMLData) Encoding() error {
 	// ...
 	yamlFile, err := os.Open(y.FileInput)
 	if err != nil {
+		fmt.Println("Не удалось открыть файл")
 		return err
 	}
 	defer func(yamlFile *os.File) {
 		err := yamlFile.Close()
 		if err != nil {
-
+			fmt.Println("Не удалось закрыть файл")
 		}
 	}(yamlFile)
 	byteValue, _ := io.ReadAll(yamlFile)
 	err = yaml.Unmarshal(byteValue, &y.DockerCompose)
 	if err != nil {
+		fmt.Println("Не удалось провести десериализацию")
 		return err
 	}
 	file, err := os.Create(y.FileOutput)
 	if err != nil {
-		os.Exit(1)
+		fmt.Println("Не удалось создать файл")
 		return err
 	}
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
-
+			fmt.Println("Не удалось закрыть файл")
 		}
 	}(file)
 	jsonData, err := json.Marshal(y.DockerCompose)
 	if err != nil {
+		fmt.Println("Не удалось провести сериализацию")
 		return err
 	}
 	_, err = file.Write(jsonData)
 	if err != nil {
+		fmt.Println("Не удалось записать данные в файл")
 		return err
 	}
 	return nil
